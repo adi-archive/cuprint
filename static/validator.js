203cd0d1f1c37ec74a474c08f50c19046de3229e
Hans E Hyttinen <hans@lumilux.org>
2011-09-22 15:10:47 -0300

    be nicer

--- static/validator.js
+++ static/validator.js
@@ -2,26 +2,26 @@ function validate_print_form(form){
 	var unire = new RegExp('\\w{2,3}\\d{1,4}');
 	
 	if(!unire.test(form.uni.value)){
-		alert('Invalid UNI');
+		alert('Please enter a valid UNI.');
 		return false;
 	}
 
 	var filere = new RegExp('\\.(pdf)|(ps)');
 
 	if(!filere.test(form.document.value)){
-		alert("Invalid file type. Only PDF and PostScript files are accepted at this time.");
+		alert("Please upload only a PDF or PostScript file.");
 		return false;
 	}
 
 	if(!form.copies.value){
-		alert("Must specify number of copies.");
+		alert("Please specify the number of copes.");
 		return false;
 	}
 
 	var pagesre = new RegExp('[\d,-]*');
 
 	if(!pagesre.test(form.pages.value)){
-		alert("Invalid page range. Must be in the form of 1,3-4 or left blank");
+		alert("Please enter a page range in the form of 1,3-4 or leave blank.");
 		return false;
 	}
 
