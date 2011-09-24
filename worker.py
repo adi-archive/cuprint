9d211e4de85ad06e05758494ad3768c930efbfa6
Zhehao Mao <zhehao.mao@gmail.com>
2011-09-23 22:37:36 -0400

    worker is working

--- worker.py
+++ worker.py
@@ -3,6 +3,32 @@ import cups
 import os
 import zmq
 import sys
+import json
+
+def deunicode(obj):
+	if isinstance(obj, dict):
+		retval = {}
+		for key, val in obj.items():
+			if(isinstance(key, unicode)):
+				key = str(key)
+			if(isinstance(val, unicode)):
+				val = str(val)
+			elif(isinstance(val, (list,dict))):
+				val = deunicode(val)
+			retval[key] = val
+		return retval
+	elif isinstance(obj, list):
+		retval = []
+		for item in obj:
+			if(isinstance(item, unicode)):
+				retval.append(str(item))
+			elif(isinstance(item, (list,dict))):
+				retval.append(deunicode(item))
+			else: retval.append(item)
+		return retval
+	else:
+		return obj
+
 
 def print_file(filename, tmp_file, printer, uni, options):
 	cups.setUser(uni)
@@ -12,7 +38,7 @@ def print_file(filename, tmp_file, printer, uni, options):
 		sys.stderr.write('File '+tmp_file+' printed successfully\n')
 		sys.stderr.flush()
 	except cups.IPPError:
-		sys.stderr.write('Printing '+tmp_file+' unsuccessful')
+		sys.stderr.write('Printing '+tmp_file+' unsuccessful\n')
 		sys.stderr.flush()
 
 COMMANDS = {'print':print_file}
@@ -27,15 +53,22 @@ def make_socket():
 def work_loop(receiver):
 	while True:
 		s = receiver.recv()
+		
+		tup = tuple(s.split(' ',1))
 
-		cmdname, datajson = tuple(s.split(' ', 1))
+		if(len(tup) != 2): continue
 
-		cmd = COMMANDS[cmdname]
+		cmdname, datajson = tup
 
-		data = json.loads(datajson)
+		cmd = COMMANDS.get(cmdname)
 
-		print('got command '+cmdname+' '+data)
-		sys.stdout.flush()
+		if not cmd:
+			print('Command '+cmdname+' not found')
+			continue
+
+		data = deunicode(json.loads(str(datajson)))
+
+		print('got command '+cmdname+' '+datajson)
 
 		if(isinstance(data, dict)):
 			cmd(**data)
