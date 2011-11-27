e8b6392cb9c5c1680f4bb50beafe16a7bc17fc7b
Zhehao Mao <zhehao.mao@gmail.com>
2011-11-27 10:28:18 -0500

    make runserver script echo pids to file

--- runserver.sh
+++ runserver.sh
@@ -1,5 +1,8 @@
 source printenv/bin/activate
 
 nohup unoconv -l &
+echo $! > pid/unoconv.pid
 nohup ./manage.py runworker &
+echo $! > pid/worker.pid
 nohup gunicorn -w 2 application:app &
+echo $! > pid/gunicorn.pid
