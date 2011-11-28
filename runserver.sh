fe25ad15a17a351becefe76313908d33413b074f
Zhehao Mao <zhehao.mao@gmail.com>
2011-11-27 21:18:46 -0500

    modify runserver.sh to start up soffice

--- runserver.sh
+++ runserver.sh
@@ -1,7 +1,16 @@
 source printenv/bin/activate
 
-nohup unoconv -l &
-echo $! > pid/unoconv.pid
+for pidfile in pid/*.pid; do
+  pid=`cat $pidfile`
+  kill $pid
+  rm $pidfile
+done
+
+# wait two seconds for processes to finish
+sleep 2
+
+nohup soffice -headless -accept="socket,port=2002;urp" -nofirststartwizard &
+echo $! > pid/soffice.pid
 nohup ./manage.py runworker &
 echo $! > pid/worker.pid
 nohup gunicorn -w 2 application:app &
